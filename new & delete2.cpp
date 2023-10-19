f2170fdfb8275cc8e4a43f88bebce8831300bd4f
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2023-10-19 14:05:18 +0530

    Update new & delete2.cpp

--- new & delete2.cpp
+++ new & delete2.cpp
@@ -3,13 +3,15 @@ using namespace std;
 
 int main(){
   
-    int *arr = new int[3];
+    int *arr = new int[4];
     arr[0] = 10;
     arr[1] = 20;
-    arr[2] = 30;
+    *(arr+2) = 30;   // another way to assign value to array element
+    arr[3] = 40;
     cout << "The value of arr[0] is " << arr[0] << endl;
     cout << "The value of arr[1] is " << arr[1] << endl;
     cout << "The value of arr[2] is " << arr[2] << endl;
+    cout << "The value of arr[3] is " << arr[3] << endl;
    
     return 0;
 }
