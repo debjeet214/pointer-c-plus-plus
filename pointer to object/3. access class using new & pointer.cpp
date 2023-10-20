ce6eccc9999918a72a651cfff16831d323ff3b3c
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2023-10-20 13:36:42 +0530

    Create 3. access class using new & pointer.cpp

---
+++ pointer to object/3. access class using new & pointer.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class complex{
+    int real, imagenary;
+    public:
+    void show(){
+        cout<<"the real part is "<<real<<endl;
+        cout<<"the imagenary part is "<<imagenary<<endl;
+    }
+    void set( int a, int b){
+        real = a;
+        imagenary = b;
+        show();
+    }
+};
+
+int main(){
+    complex *p = new complex;
+    (*p).set(3, 12);
+}
